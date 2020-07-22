5c3a2ed2c425c66fee1b05ee047d9401f194c223
Musfirotus Sa'adah <musfirotus@gmail.com>
2020-07-22 08:55:01 +0700

    code by musfirotus

--- 006.js
+++ 006.js
@@ -1,3 +1,11 @@
+//code by musfirotus
+
 let str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const words =
-    filter.addWords(...str);
+
+// function rep(str) {
+//     const char = str.length + "*";
+//     return str.replace(/dolor|elit|quis|nisi|fugiat|proident|laborum/gi, char)
+// }
+
+strg = str.replace(/dolor|elit|quis|nisi|fugiat|proident|laborum/gi, "*");
+console.log(strg);
