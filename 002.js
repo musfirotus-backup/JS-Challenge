ad303fe4cdd80f77e4c8cf9b71afac0e53bfdb41
Musfirotus <musfirotus@gmail.com>
2020-07-29 07:03:22 +0700

    re-create no. 2

--- 002.js
+++ 002.js
@@ -4,6 +4,8 @@ const third = ['rolling', 'her', 'eyes'];
 
 //.concat() untuk menggabungkan 3 array
 //.join() unuk menggabungkan semua value array
-const joinAll = first.concat(second, third).join(" ");
-
+const joinAll = first.concat(second, third).join(" "); // first option
 console.log(joinAll + ".");
+
+const allJoin = [...first, ...second, ...third] // second option
+console.log(allJoin.join(" ") + ".");
